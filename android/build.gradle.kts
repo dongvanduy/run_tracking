8f3c389d1077f0c1da2374d8063fceec6a55c2e7
DongDuy <58018875+dongvanduy@users.noreply.github.com>
2026-01-28 08:46:00 +0700

    Add namespace fallback for isar_flutter_libs

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 allprojects {
     repositories {
         google()
@@ -17,6 +19,18 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        if (name == "isar_flutter_libs") {
+            extensions.findByType(LibraryExtension::class.java)?.apply {
+                if (namespace == null) {
+                    namespace = "dev.isar.isar_flutter_libs"
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
